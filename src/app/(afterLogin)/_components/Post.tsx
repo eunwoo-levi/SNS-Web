d419f09c30f9b75c202cd728ec6d039a8a81926a
eunwoo-levi <eunwoo1341@gmail.com>
2024-08-16 07:02:45 +0900

    feat:Card component

--- src/app/(afterLogin)/_components/Post.tsx
+++ src/app/(afterLogin)/_components/Post.tsx
@@ -1,3 +1,54 @@
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.locale("en");
+dayjs.extend(relativeTime);
+
 export default function Post() {
-  return <div>Post</div>;
+  const target = {
+    User: {
+      id: "eunwoo1234",
+      nickname: "Eunwoo",
+      image: "/user2.jpg",
+    },
+    content: "나만의 SNS 페이지 만들어 보는중..!",
+    createAt: new Date(),
+    Image: "/contentImg.png",
+  };
+
+  return (
+    <article className="flex flex-col py-[12px] px-[16px] border-b-[1px] border-[rgb(47,51,54)] cursor-pointer hover:bg-[rgba(0,0,0,0.03)] dark:hover:bg-[rgba(0,0,0,0.2)] transition duration-200">
+      <div className="flex flex-row">
+        <Link href={`/${target.User.id}`} className="w-[40px] mr-[12px]">
+          <Image
+            width={40}
+            height={40}
+            src={target.User.image}
+            alt={target.User.nickname}
+            className="rounded-full hover:shadow-lg "
+          />
+        </Link>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row">
+            <Link href={`${target.User.id}`}>
+              <span className="hover:underline">{target.User.nickname}</span>
+              <span className="text-neutral-400 ml-2">@{target.User.id}</span>
+            </Link>
+            <span className="text-neutral-400 ml-2 hover:underline">
+              {dayjs(target.createAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <Image
+            width={600}
+            height={400}
+            src={target.Image}
+            alt="content Image"
+          />
+        </div>
+      </div>
+    </article>
+  );
 }
